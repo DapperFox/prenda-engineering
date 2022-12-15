ca401566baacd9d3f27a7fd04f6e90829b99f8b0
Blake Bishop <blakesterbishop@gmail.com>
2022-12-15 16:14:49 -0700

    Deploy website - based on f4073729b977141530ced997a342c89f8ca11928

--- assets/js/c040153c.e1035a99.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkprenda_engineering=self.webpackChunkprenda_engineering||[]).push([[9604],{5359:e=>{e.exports=JSON.parse('{"label":"hello","permalink":"/prenda-engineering/blog/tags/hello","allTagsPath":"/prenda-engineering/blog/tags","count":2}')}}]);
--- assets/js/da5fc1a1.2d0f7c6c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkprenda_engineering=self.webpackChunkprenda_engineering||[]).push([[2156],{8597:e=>{e.exports=JSON.parse('{"permalink":"/prenda-engineering/blog/tags/facebook","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
